99e72a6441d65ebd8408e83c24a333c9ac04aa2e
somshivgupta <59904984+somshivgupta@users.noreply.github.com>
2023-09-10 11:27:39 +0530

    Update q26.c

--- Hands_on_list1/q26.c
+++ Hands_on_list1/q26.c
@@ -2,6 +2,7 @@
 ============================================================================
 Name : q26.c
 Author : Som Shiv Gupta
+Roll no: MT2023015
 Description :Write a program to execute an executable program.
              a. use some executable program
              b. pass some input to an executable program. (for example execute an executable of $./a.out name)
